846615f6067d5470906eb2b14b4f70aa8ad06fbe
blaffy <ellobas@mail.ru>
2025-05-26 20:08:33 +0300

    123

--- Form1.cs
+++ Form1.cs
@@ -18,23 +18,28 @@ namespace WinFormsApp1
         {
             using (lab8YazikiContext db = new lab8YazikiContext())
             {
-                orderGridView.DataSource = db.Orders.Select(d => new { Id = d.Id, Amount = d.Amount, Date = d.OrderDate, Product = d.Product.Name, Sum = d.Amount * d.Product.Price }).ToList();
+                orderGridView.DataSource = db.Orders.Select(d => new { 
+                    Id = d.Id, 
+                    Amount = d.Amount, 
+                    Date = d.OrderDate, 
+                    Product = d.Product.Name, 
+                    Sum = d.Amount * d.Product.Price 
+                }).ToList();
 
                 orderGridView!.Columns["Id"]!.Visible = false;
                 orderGridView!.Columns["Id"]!.DisplayIndex = 0;
 
                 orderGridView!.Columns["Amount"]!.DisplayIndex = 3;
-                orderGridView!.Columns["Amount"]!.HeaderText = "�����";
+                orderGridView!.Columns["Amount"]!.HeaderText = "Количество";
 
                 orderGridView!.Columns["Date"]!.DisplayIndex = 2;
-                orderGridView!.Columns["Date"]!.HeaderText = "����";
+                orderGridView!.Columns["Date"]!.HeaderText = "Дата";
 
                 orderGridView!.Columns["Product"]!.DisplayIndex = 1;
-                orderGridView!.Columns["Product"]!.HeaderText = "�����";
+                orderGridView!.Columns["Product"]!.HeaderText = "Продукт";
 
                 orderGridView.Refresh();
             }
-
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -57,6 +62,7 @@ namespace WinFormsApp1
         private void orderGridView_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
         {
             try {
+                if (e.Button == MouseButtons.Right)
                 orderGridView.CurrentCell = orderGridView.Rows[e.RowIndex].Cells[e.ColumnIndex];
                 orderGridView.Rows[e.RowIndex].Selected = true;
                 orderGridView.Focus();
